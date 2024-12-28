aa4eee61c9fa9af8ee68bc137e9ddba113d0ef0c
festusrono <festusrono56@gmail.com>
2024-12-28 16:54:10 +0100

    use case update

--- shared/src/commonMain/kotlin/festus/rono/weatherapp/domain/model/use_case/GetCurrentWeatherInfoUseCase.kt
+++ shared/src/commonMain/kotlin/festus/rono/weatherapp/domain/model/use_case/GetCurrentWeatherInfoUseCase.kt
@@ -1,7 +1,15 @@
 package festus.rono.weatherapp.domain.model.use_case
 
+import festus.rono.weatherapp.domain.model.Weather
 import festus.rono.weatherapp.domain.model.respository.WeatherRepository
 
 class GetCurrentWeatherInfoUseCase(private val weatherRepository: WeatherRepository) {
-    suspend operator fun invoke(lat)
+    suspend operator fun invoke(lat: Double, long: Double) : Result<Weather> {
+        return try {
+            val response = weatherRepository.getCurrentWeatherInfo(lat, long)
+            Result.success(response)
+        }catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
